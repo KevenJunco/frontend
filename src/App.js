5bbdfa8ce99876a15b40340c64935df4a1be66fa
Keven Junco <kevenjunco@hotmail.com>
2020-04-23 09:58:30 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import api from './services/api';
 
 import Routes from './routes';
 import GlobalStyle from './styles/global';
---
+++ src/pages/repository/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+  }
+`;
